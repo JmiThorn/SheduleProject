8f454068ba8b157bd989ea91ef793698d320437b
rus_inc <sovist2000@gmail.com>
2021-06-27 18:37:52 +0300

    BuildingConverter вынесен в utils Теперь этот конвертер используется и для списка адуиторий Новый формат сообщения об ошибке Обновленная либка

--- Shedule/Controls/MainScheduleItemControl.xaml.cs
+++ Shedule/Controls/MainScheduleItemControl.xaml.cs
@@ -47,35 +47,5 @@ namespace Shedule.Controls
         }
     }
 
-    public class BuildingConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value == null)
-                return "";
-            if(value is int)
-            {
-                if((int)value == 1)
-                {
-                    return "Старый";
-                }else if((int)value == 2)
-                {
-                    return "Новый";
-                }
-                else
-                {
-                    return "Неизвестный";
-                }
-            }
-            else
-            {
-                return "Неизвестный";
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return DependencyProperty.UnsetValue;
-        }
-    }
+    
 }
--- Shedule/Utils/AppUtils.cs
+++ Shedule/Utils/AppUtils.cs
@@ -24,38 +24,46 @@ namespace Shedule.Utils
                 switch (e.ErrorCode)
                 {
                     case LibraryErrorException.ErrorCodes.AUTH_ERROR:
-                        if(MessageBox.Show("Укажите корректные логин и пароль в настройках!\n\nПерейти в настройки?", "Ошибка авторизации!", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
+                        if (MessageBox.Show("Укажите корректные логин и пароль в настройках!\n\nПерейти в настройки?", "Ошибка авторизации!", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
                         {
                             MainWindow.Instance.MainFrame.NavigationService.Navigate(new Settings());
                         }
-                        
+
                         break;
                     case LibraryErrorException.ErrorCodes.ENDPOINT_ERROR:
-                        if(MessageBox.Show("Сервер не указан или недоступен!\nУкажите корректный адрес и порт в настройках или обратитесь к администратору\n\nПерейти в настройки?", "Ошибка соединения!", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
+                        if (MessageBox.Show("Сервер не указан или недоступен!\nУкажите корректный адрес и порт в настройках или обратитесь к администратору\n\nПерейти в настройки?", "Ошибка соединения!", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
                         {
                             MainWindow.Instance.MainFrame.NavigationService.Navigate(new Settings());
                         }
                         ;
                         break;
                     case LibraryErrorException.ErrorCodes.UNKNOWN_SERVER_ERROR:
-                        MessageBox.Show("При попытке соединения с сервером возникла следующая ошибка:\n" + e.Message, "Серверная ошибка!",MessageBoxButton.OK,MessageBoxImage.Error);
+                        if (MessageBox.Show("При попытке соединения с сервером возникла следующая ошибка:\n\n" + e.FormattedMessage + "\n\nПоказать полную версию сообщения об ошибке?", "Серверная ошибка!", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
+                        {
+                            MessageBox.Show(e.FullMessage, "Подробности ошибки", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
                         break;
                 }
                 throw;
             }
             catch (ServerErrorException e)
             {
-                MessageBox.Show("При попытке соединения с сервером возникла следующая ошибка:\n" + e.Message, "Серверная ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                if(MessageBox.Show("При попытке соединения с сервером возникла следующая ошибка:\n\n" + e.FormattedMessage + "\n\nПоказать полную версию сообщения об ошибке?", "Серверная ошибка!", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes){
+                    MessageBox.Show(e.FullMessage, "Подробности ошибки", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
                 throw;
             }
             catch (RequestErrorException e)
             {
-                MessageBox.Show("При отправке запрроса произошла следующая ошибка:\n" + e.Message, "Ошибка данных!", MessageBoxButton.OK, MessageBoxImage.Error);
+                if(MessageBox.Show("При отправке запрроса произошла следующая ошибка:\n\n" + e.FormattedMessage + "\n\nПоказать полную версию сообщения об ошибке?", "Ошибка данных!", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show(e.FullMessage, "Подробности ошибки", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
                 throw;
             }
             catch (Exception e)
             {
-                MessageBox.Show("При выполнении программы возникла следующая ошибка:\n" + e.Message, "Ошибка приложения!", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("При выполнении программы возникла следующая ошибка:\n\n" + e.Message, "Ошибка приложения!", MessageBoxButton.OK, MessageBoxImage.Error);
                 throw;
             }
             
